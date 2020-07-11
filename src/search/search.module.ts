9f0cc6fcd9c43985bbcb1ef572ff96bc123214f1
Sahus Nulu <53015701+Insainian@users.noreply.github.com>
2020-07-11 15:47:35 -0700

    Integrate Elasticsearch client

---
+++ src/search/search.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { ElasticsearchModule } from '@nestjs/elasticsearch'
+import { SearchService } from './search.service';
+
+@Module({
+    imports: [ElasticsearchModule.register({
+        node: 'http://localhost:9200',
+    })],
+    providers: [SearchService]
+})
+export class SearchModule { }
